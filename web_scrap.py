3a762b302dcaf25c404acfd7cc2896f575286323
Carlos Augusto Calage <calage.carlos@gmail.com>
2021-11-17 00:33:08 -0300

    Implemented loop for getting data

--- web_scrap.py
+++ web_scrap.py
@@ -1,12 +1,11 @@
 import pandas as pd
+import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
-url="https://www.sweetwater.com/c987--Classical_and_Nylon_String_Guitars"
 
 #Configurations for hidding that we are actually a bot :´)
 options = Options()
@@ -15,8 +14,6 @@ options.add_argument('--start-maximized')
 options.add_argument('--start-fullscreen')
 options.add_argument('--single-process')
 options.add_argument('--disable-dev-shm-usage')
-options.add_argument("--incognito")
-options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_experimental_option('useAutomationExtension', False)
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -30,6 +27,9 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
                 "delete newProto.webdriver;"
                 "navigator.__proto__ = newProto;"
         })
+driver.maximize_window()
+
+url="https://www.sweetwater.com/c987--Classical_and_Nylon_String_Guitars"
 
 # Start the process of entering the page
 driver.get(url)
@@ -40,11 +40,34 @@ driver.find_element(By.XPATH, "//span[@class='site-contact-preview__close']").cl
 # If the cookie check box is still there, a human should do the job
 WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.XPATH, "//div[@id='ccpa-cookie']")))
 
-# Getting all table rows
-table = driver.find_elements(By.XPATH, "//li[@class='table__row']")
+products = driver.find_elements(By.XPATH, "//h2[@class='product-card__name']//a")
 
-data = {}
+index = 14
+q = range(len(products))
 
-# Getting header and body and appending to data dict
-for i in table:
-  data[i.find_element(By.CLASS_NAME, 'table__header').text.replace(':', '')] = i.find_element(By.CLASS_NAME, 'table__cell').text
+for elem in q:
+  data = {}
+  
+  time.sleep(1)
+  
+  products[index].click()
+
+  time.sleep(1)
+
+  dollars = driver.find_element(By.TAG_NAME, 'dollars').text
+  cents = driver.find_element(By.TAG_NAME, 'cents').text
+  data['Price']  = dollars + cents
+
+  # Getting all table rows
+  table = driver.find_elements(By.XPATH, "//li[@class='table__row']")
+
+  # Getting header and body and appending to data dict
+  for i in table:
+    data[i.find_element(By.CLASS_NAME, 'table__header').text.replace(':', '')] = i.find_element(By.CLASS_NAME, 'table__cell').text
+
+  print(data)
+
+  driver.back()
+  time.sleep(1)
+  products = driver.find_elements(By.XPATH, "//h2[@class='product-card__name']//a")
+  index += 1
